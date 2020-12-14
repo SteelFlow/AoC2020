5563d8ed5beed0ef6e82d7b13c2d1624bf32f0d5
SteelFlow <staale.stromme@gmail.com>
2020-12-14 15:48:20 +0100

    part 1

--- src/main/scala/AoC04.scala
+++ src/main/scala/AoC04.scala
@@ -3,18 +3,66 @@ object AoC04 extends App {
 
   def getNextPassportBatch(
       input: List[String],
-      fromIndex: Int,
-      currentIndex: Int
-  ): (Int, Int) = {
-
-    if (currentIndex == input.length - 1) {
-      (fromIndex, currentIndex)
+      fromIndex: Int = 0,
+      currentIndex: Int = 0
+  ): (Int, Int, String) = {
+    if (currentIndex == input.length) {
+      (
+        fromIndex,
+        currentIndex - 1,
+        input.slice(fromIndex, currentIndex).mkString(" ")
+      )
     } else {
       input(currentIndex) match {
-        case x if x.isBlank => (fromIndex, currentIndex)
-        case _              => getNextPassportBatch(input, fromIndex, currentIndex + 1)
+        case x if x.isBlank =>
+          (
+            fromIndex,
+            currentIndex,
+            input.slice(fromIndex, currentIndex).mkString(" ")
+          )
+        case _ => getNextPassportBatch(input, fromIndex, currentIndex + 1)
       }
     }
   }
+
+  def getBatchedPassports(
+      input: List[String],
+      startIndex: Int = 0,
+      currentIndex: Int = 0,
+      batchedPassportList: List[String] = List.empty
+  ): List[String] = {
+    if (startIndex >= input.length) {
+      batchedPassportList
+    } else {
+      val batch = getNextPassportBatch(input, startIndex, currentIndex)
+      getBatchedPassports(
+        input,
+        batch._2 + 1,
+        batch._2 + 1,
+        batchedPassportList :+ batch._3
+      )
+    }
+  }
+
+  def parsePassport(passport: String) {
+    passport
+  }
+
+  val input = LoadInput.load04()
+
+  val res = getBatchedPassports(input)
+    .filter(passport => {
+      passport.contains("byr") &&
+        passport.contains("iyr") &&
+        passport.contains("eyr") &&
+        passport.contains("hgt") &&
+        passport.contains("hcl") &&
+        passport.contains("ecl") &&
+        passport.contains("pid")
+    })
+    .map(parsePassport)
+    .length
+
+  println(s"Advent of Code 2020 04 part 1: $res")
 
 }
--- src/test/scala/AoC04Test.scala
+++ src/test/scala/AoC04Test.scala
@@ -6,10 +6,13 @@ class AoC04Test extends AnyFunSuite {
   test("Test get next batch first batch") {
     val input = LoadInput.load04()
 
-    val res = AoC04.getNextPassportBatch(input, 0, 0)
+    val res = AoC04.getNextPassportBatch(input)
 
-    assert(0 === res._1)
-    assert(2 === res._2)
+    val expected =
+      "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm"
+    assert(res._1 === 0)
+    assert(res._2 === 2)
+    assert(res._3 === expected)
 
   }
 
@@ -18,8 +21,12 @@ class AoC04Test extends AnyFunSuite {
 
     val res = AoC04.getNextPassportBatch(input, 6, 6)
 
-    assert(6 === res._1)
-    assert(10 === res._2)
+    val expected =
+      "hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm"
+
+    assert(res._1 === 6)
+    assert(res._2 === 10)
+    assert(res._3 === expected)
 
   }
 
@@ -28,9 +35,19 @@ class AoC04Test extends AnyFunSuite {
 
     val res = AoC04.getNextPassportBatch(input, 11, 11)
 
-    assert(11 === res._1)
-    assert(12 === res._2)
+    val expected =
+      "hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in"
 
+    assert(res._1 === 11)
+    assert(res._2 === 12)
+    assert(res._3 === expected)
   }
 
+  test("Get batched passports") {
+    val input = LoadInput.load04()
+
+    val res = AoC04.getBatchedPassports(input)
+
+    assert(res.length === 4)
+  }
 }
